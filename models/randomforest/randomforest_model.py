99619b602fd7f366a7a171af608b1de1b0c975c8
choy-qi-hui <choyqihui@u.nus.edu>
2025-11-16 17:58:54 +0800

    updated rf model and reports

---
+++ models/randomforest/randomforest_model.py
@@ -0,0 +1,126 @@
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import roc_auc_score, average_precision_score, precision_score, recall_score, f1_score
+from sklearn.feature_selection import RFE
+import os
+import joblib
+import itertools
+
+# File path to store model, best models & features
+ROOT_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), '..', '..'))
+DATA_DIR = os.path.join(ROOT_DIR, 'data', 'interim')
+MODEL_DIR = os.path.join(ROOT_DIR, 'models', 'randomforest')
+REPORT_DIR = os.path.join(ROOT_DIR, 'reports', 'randomforest')
+
+# Make sure directories exist
+os.makedirs(MODEL_DIR, exist_ok=True)
+os.makedirs(REPORT_DIR, exist_ok=True)
+
+train_df = pd.read_csv(os.path.join(DATA_DIR, 'twitter_train_processed.csv'), index_col=0)
+val_df = pd.read_csv(os.path.join(DATA_DIR, 'twitter_val_processed.csv'), index_col=0)
+test_df = pd.read_csv(os.path.join(DATA_DIR, 'twitter_test_processed.csv'), index_col=0)
+
+X_train = train_df.drop(columns=['account_type'])
+y_train = train_df['account_type']
+
+X_val = val_df.drop(columns=['account_type'])
+y_val = val_df['account_type']
+
+X_test = test_df.drop(columns=['account_type'])
+y_test = test_df['account_type']
+
+print(f"Train size: {len(X_train)}, Validation size: {len(X_val)}, Test size: {len(X_test)}")
+
+dataset_name = "original" # change based on training set
+
+# Random Forest Evaluation
+def evaluate(model, X, y):
+    y_pred = model.predict(X)
+    y_prob = model.predict_proba(X)[:, 1]
+
+    auc = roc_auc_score(y, y_prob)
+    pr_auc = average_precision_score(y, y_prob)
+    precision = precision_score(y, y_pred)
+    recall = recall_score(y, y_pred)
+    f1 = f1_score(y, y_pred)
+
+    print(f"\n{dataset_name} Set Metrics")
+    print(f"AUC: {auc:.4f}")
+    print(f"PR-AUC: {pr_auc:.4f}")
+    print(f"Precision: {precision:.4f}")
+    print(f"Recall: {recall:.4f}")
+    print(f"F1-score: {f1:.4f}")
+
+    return auc, pr_auc, precision, recall, f1
+
+best_recall_score = 0
+best_model_params = None
+best_model = None
+
+param_dist = {
+    "n_estimators": [100, 200, 300],
+    "max_depth": [None, 10, 20],
+    "min_samples_split": [2, 5],
+    "min_samples_leaf": [1, 2],
+    "max_features": ["sqrt", None],
+    "bootstrap": [True, False]
+}
+
+# Generate all combinations of hyperparameters
+for combo in itertools.product(
+    param_dist["n_estimators"],
+    param_dist["max_depth"],
+    param_dist["min_samples_split"],
+    param_dist["min_samples_leaf"],
+    param_dist["max_features"],
+    param_dist["bootstrap"]
+):
+
+    params = {
+        "n_estimators": combo[0],
+        "max_depth": combo[1],
+        "min_samples_split": combo[2],
+        "min_samples_leaf": combo[3],
+        "max_features": combo[4],
+        "bootstrap": combo[5]
+    }
+
+    base_rf = RandomForestClassifier(**params, random_state=42, n_jobs=-1)
+    
+    # True RFE
+    rfe = RFE(estimator=base_rf, n_features_to_select=round(X_train.shape[1]*0.7), step=0.1)
+    rfe.fit(X_train, y_train)
+    
+    X_train_rfe = X_train.loc[:, rfe.support_]
+    X_val_rfe = X_val.loc[:, rfe.support_]
+
+    model = RandomForestClassifier(**params, random_state=42, n_jobs=-1)
+    model.fit(X_train_rfe, y_train)
+    
+    _, _, _, recall, _ = evaluate(model, X_val_rfe, y_val)
+
+    if recall > best_recall_score:
+        best_recall_score = recall
+        best_model_params = params
+        best_model = model
+        best_features = X_train.columns[rfe.support_]
+
+print("\nBest hyperparameters:")
+print(best_model_params)
+
+# Refit best model on train+val sets
+X_train_full = pd.concat([X_train, X_val])
+y_train_full = pd.concat([y_train, y_val])
+
+# Use only selected features
+X_train_full_rfe = X_train_full[best_features]
+X_test_rfe = X_test[best_features]
+
+refitted_model = RandomForestClassifier(**best_model_params, random_state=42, n_jobs=-1)
+refitted_model.fit(X_train_full_rfe, y_train_full)
+
+# Save the refitted model
+joblib.dump(refitted_model, os.path.join(REPORT_DIR, f'best_rf_rfe_{dataset_name}.joblib'))
+joblib.dump(best_features, os.path.join(REPORT_DIR, f'best_features_rfe_{dataset_name}.joblib'))
+
+
